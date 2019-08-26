7f5d81ce36e39393e52b8085f01bff5c782a583b
XorinNebulas <34551431+XorinNebulas@users.noreply.github.com>
2019-08-26 13:09:26 -0700

    add if statment in renderList function

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -27,20 +27,22 @@ class StreamList extends React.Component {
   }
 
   renderList() {
-    return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            <Link to={`/streams/${stream.id}`} className="header">
-              {stream.title}
-            </Link>
-            <div className="description">{stream.description}</div>
+    if (this.props.streams) {
+      return this.props.streams.map(stream => {
+        return (
+          <div className="item" key={stream.id}>
+            {this.renderAdmin(stream)}
+            <i className="large middle aligned icon camera" />
+            <div className="content">
+              <Link to={`/streams/${stream.id}`} className="header">
+                {stream.title}
+              </Link>
+              <div className="description">{stream.description}</div>
+            </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
+    }
   }
 
   renderCreate() {
